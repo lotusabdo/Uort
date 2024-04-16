442173169a8b5458df1c1b1878891927dc8fea0a
Abdo Lotus <157813008+lotusabdo@users.noreply.github.com>
2024-04-16 22:37:41 +0200

    Update start.py

--- StringGenBot/start.py
+++ StringGenBot/start.py
@@ -12,27 +12,27 @@ async def start(bot: Client, msg: Message):
     me2 = (await bot.get_me()).mention
     await bot.send_message(
         chat_id=msg.chat.id,
-        text=f"""¤¦ اهلا بـك عزيـزي 
+        text=f"""• اهلا بـك عزيـزي 
 
-¤¦ يمكنك استـخـراج التالـي
+• يمكنك استـخـراج التالـي
 
-¤¦ تيرمڪس تليثون للحسابات
+• تيرمڪس تليثون للحسابات
 
-¤¦ تيرمـكـس تليثون للبوتـات
+• تيرمـكـس تليثون للبوتـات
 
-¤¦ بايـروجـرام مـيوزك للحسابات
+• بايـروجـرام مـيوزك للحسابات
 
-¤¦ بايـروجـرام مـيوزك للبوتات
+• بايـروجـرام مـيوزك للبوتات
 
-¤¦ تم انشاء البوت بواسطة [ㅤ𓏺˛ َ˛ 𖥻 𝐔𝐑 , 𝐅𝐀𝐕 𝐕𝐎𝐃𝐄𝐈𝐊𝐀• ˼](https://t.me/EU_TM)""",
+• تم انشاء البوت بواسطة [𖥻 𝐔𝐑 , 𝐅𝐚𝐕 𝐀𝐛𝐃𝐎𝐨 -](https://t.me/EU_TM)""",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(text="إضغط لبدا استخراج الكود", callback_data="generate")
                 ],
                 [
-                    InlineKeyboardButton(" sᴏᴜʀᴄᴇ •", url="https://t.me/l2_2Y"),
-                    InlineKeyboardButton(" ᴅᴇᴠᴇʟᴏᴩᴇʀ •", url="https://t.me/EU_TM"),
+                    InlineKeyboardButton("𝐂𝐇𝐀𝐍𝐍𝐄𝐋", url="https://t.me/l2_2Y"),
+                    InlineKeyboardButton(" 𖥻 𝐔𝐑 , 𝐅𝐚𝐕 𝐀𝐛𝐃𝐎𝐨 - ", url="https://t.me/EU_TM"),
                 ]
             ]
         ),
